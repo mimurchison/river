bf4413fadfa19867a916c04bab65ded7c6b9ca22
David Hariri <david.hariri@gmail.com>
2014-01-13 23:42:08 -0500

    got some D3 going

---
+++ data/stats.php
@@ -0,0 +1,74 @@
+<?php
+header('Content-Type: application/json');
+
+include "../pass.php";
+//stats getter
+function get_data($query, $dbhost, $dbuser, $dbpass, $dbname){
+	$db = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+
+	if($db->connect_errno > 0){
+	    die('Unable to connect to database [' . $db->connect_error . ']');
+	}
+
+	if(!$result = $db->query($query)){
+	    die('There was an error running the query [' . $db->error . ']');
+	}
+
+	$db->close();
+
+	return $result;
+}
+
+$all_data = array();
+
+//service data
+$query = "SELECT service, COUNT(*) FROM tbl_posts GROUP BY service";
+$service_stats = get_data($query, $dbhost, $dbuser, $dbpass, $dbname);
+
+$service_data = [];
+
+foreach ($service_stats as $key => $value) {
+	$service_data[$value['service']] = $value['COUNT(*)'];
+}
+array_push($all_data, $service_data);
+
+
+//category data
+$query = "SELECT category, COUNT(*) FROM tbl_posts GROUP BY category";
+$category_stats = get_data($query, $dbhost, $dbuser, $dbpass, $dbname);
+
+$category_data = array();
+
+foreach ($category_stats as $key => $value) {
+	$category_data[$value['category']] = $value['COUNT(*)'];
+}
+array_push($all_data, $category_data);
+
+//service data by day of week
+$query = "SELECT DAYNAME(datetime) AS DayWeek, COUNT(service) AS Count FROM tbl_posts GROUP BY DAYNAME(datetime) ORDER BY DAYNAME(datetime) Desc";
+$serviceWeekDay_stats = get_data($query, $dbhost, $dbuser, $dbpass, $dbname);
+
+$serviceWeekDay_data = array();
+
+foreach ($serviceWeekDay_stats as $key => $value) {
+	$serviceWeekDay_data[$value['DayWeek']] = $value['Count'];
+}
+
+array_push($all_data, $serviceWeekDay_data);
+
+//service data by day
+$query = "SELECT DATE(datetime) AS Date, COUNT(service) AS Count FROM tbl_posts GROUP BY DATE(datetime) ORDER BY datetime Desc";
+$serviceDay_stats = get_data($query, $dbhost, $dbuser, $dbpass, $dbname);
+
+$serviceDay_data = array();
+
+foreach ($serviceDay_stats as $key => $value) {
+	$serviceDay_data[$value['Date']] = $value['Count'];
+}
+
+array_push($all_data, $serviceDay_data);
+
+$all_data = preg_replace( "/\"(\d+)\"/", '$1', json_encode($all_data, true) );
+echo $all_data;
+
+?>
--- index.php
+++ index.php
@@ -30,22 +30,90 @@ http://www.dhariri.com
     <!-- Scripts -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="js/stroll.js"></script>
+    <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
 </head>
 <body>
+    <div class="stats">
+        <div class="stats-box">
+        </div>
+    </div>
     <ul id="content" class="posts flip">
         <div class="loading"></div>
     </ul>
 </body>
 
-<script>
+<script type="text/javascript">
+
+$('.stats').click(function() {
+    if($(this).height() > 50) {
+        $(this).removeClass('open').addClass('closed');
+    } else {
+        $(this).removeClass('closed').addClass('open');
+    }
+});
+
 $(document).ready(function() {
     $('#content').load("views/feed.php", function() {
         $( this ).hide().fadeIn('slow');
+
+        //Prevent mobile from loading unneccesarily
         if($(window).width() > 600) {
             stroll.bind( 'ul' );
+
+            $.getJSON( "data/stats.php", function( data ) {
+                var serviceVals = [];
+                var categoryVals = [];
+
+                $.each( data[0], function( key, val ) {
+                    //Service Data
+                    serviceVals.push ( val );
+                });
+
+                $.each( data[1], function( key, val ) {
+                    //Category Data
+                    categoryVals.push ( val );
+                });
+
+                var serviceData = [serviceVals];
+                var categoryData = [categoryVals];
+
+                var m = 10,
+                    r = 100,
+                    z = d3.scale.category20c();
+
+                var svg = d3.select(".stats-box").selectAll("svg")
+                .data(serviceData)
+                .enter().append("svg:svg")
+                .attr("width", (r + m) * 2)
+                .attr("height", (r + m) * 2)
+                .append("svg:g")
+                .attr("transform", "translate(" + (r + m) + "," + (r + m) + ")");
+
+                svg.selectAll("path")
+                .data(d3.layout.pie())
+                .enter().append("svg:path")
+                .attr("d", d3.svg.arc()
+                .innerRadius(r / 2)
+                .outerRadius(r))
+                .style("fill", function(d, i) { 
+                    switch(i) {
+                        case 0:
+                            return "#EA4C89";
+                            break;
+                        case 1:
+                            return "#2ecc71";
+                            break;
+                        case 2:
+                            return "#E1DDD3";
+                            break;
+                        case 3:
+                            return "#55ACEE";
+                            break;
+                    }
+                });
+            });
         }
     });
 });
 </script>
-
 </html>
