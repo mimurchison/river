01ec563171626d1fd933c7e0bd229d0f36b88fc2
David Hariri <david.hariri@gmail.com>
2014-01-14 21:13:42 -0500

    added pass

---
+++ pass.php
@@ -0,0 +1,25 @@
+<?php
+
+//Usernames Etc...
+$twitter_handle = "YOUR_TWITTER_HANDLE";
+$instagram_id = "YOUR_INSTAGRAM_ID";
+$dribbble_name = "YOUR_DRIBBBLE_NAME";
+$github_name = "YOUR_GITHUB_NAME";
+
+//DB Access
+$dbhost = "your-database-host-url";
+$dbuser = "your-database-username";
+$dbpass = "your-database-password";
+$dbname = "your-database-name";
+
+//This is for twitter api access:
+$settings = array(
+    'oauth_access_token' => "ACCESS_TOKEN",
+    'oauth_access_token_secret' => "ACCESS_TOKEN_SECRET",
+    'consumer_key' => "CONSUMER_KEY",
+    'consumer_secret' => "CONSUMER_SECRET"
+);
+
+$instagram_client_id = "INSTAGRAM-CLIENT-ID";
+
+?>
